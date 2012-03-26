92e2f9da73b00585ed8f66d854c34a0715e00db9
Hao Zhang <hzhang.eng@gmail.com>
2012-03-26 20:11:24 +0800

    .

--- init.el
+++ init.el
@@ -6,3 +6,7 @@
 
 ;; automatically revert buffer when the file updates.
 (global-auto-revert-mode 1)
+
+;; global key set
+(global-set-key [f3]   'find-file-at-point)
+(global-set-key [f12]   'ff-find-other-file)
